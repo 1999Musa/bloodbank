5631e3e9e534eaad6eab9d9788d72e94a9cad363
1999Musa <113199770+1999Musa@users.noreply.github.com>
2022-12-10 18:37:25 +0600

    Delete ProjectBloodBank directory

--- ProjectBloodBank/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.projectbloodbank {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.projectbloodbank to javafx.fxml;
-    exports com.example.projectbloodbank;
-}
